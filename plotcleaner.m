392e08fbea1b68f57410309b0abc517afa466540
ericbumbalough <eric.bumbalough@gmail.com>
2021-07-08 16:25:40 -0400

    Delete plotcleaner.m

--- plotcleaner.m
+++
@@ -1,6 +0,0 @@
-% plot maker: cleans up plot
-set(gcf, 'PaperType','tabloid')
-set(gca,'XTick',[],'YTick',[],'Xcolor','w','Ycolor','w')
-axis tight
-set(gca,'ylim',1.01*get(gca,'ylim'))
-set(gca,'xlim',1.01*get(gca,'xlim'))
